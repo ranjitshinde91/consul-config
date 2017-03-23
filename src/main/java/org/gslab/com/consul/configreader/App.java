48f4e5c99d86be6b0ae8fb7df40ecd5cd6b59dd7
Ranjit <ranjit.shinde@gslab.com>
2017-03-23 15:00:54 +0530

    cfg4j integration

--- src/main/java/org/gslab/com/consul/configreader/App.java
+++ src/main/java/org/gslab/com/consul/configreader/App.java
@@ -5,11 +5,13 @@ public class App {
 	public static void main(String[] args) {
 		
 		ConfigReader reader = new ConfigReader();
-		Config config = reader.readConfigurationFromConsul("com.gslab.conf.test");
-		System.out.println(config.firstName()+" - "+config.lastName());
+		Config consulCnfig = reader.readConfigurationFromConsul("com.gslab.conf.test");
+		System.out.println(consulCnfig.firstName()+" - "+consulCnfig.lastName());
 		
-		Config config2 = reader.readConfigurationFromFile("C://Users/GS-0639/ranjit/consul-demo/src/main/resources/test.yaml");
-		System.out.println(config2.firstName()+" - "+config2.lastName());
+		Config fileConfig = reader.readConfigurationFromFile("C://Users/GS-0639/ranjit/consul-demo/src/main/resources/test.yaml");
+		System.out.println(fileConfig.firstName()+" - "+fileConfig.lastName());
 		
+		Config classpathConfig = reader.readConfigurationFromClassPath("test.yaml");
+		System.out.println(classpathConfig.firstName()+" - "+classpathConfig.lastName());
 	}
 }
--- src/main/java/org/gslab/com/consul/configreader/ConfigReader.java
+++ src/main/java/org/gslab/com/consul/configreader/ConfigReader.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import org.cfg4j.provider.ConfigurationProvider;
 import org.cfg4j.provider.ConfigurationProviderBuilder;
 import org.cfg4j.source.ConfigurationSource;
+import org.cfg4j.source.classpath.ClasspathConfigurationSource;
 import org.cfg4j.source.context.filesprovider.*;
 import org.cfg4j.source.consul.ConsulConfigurationSourceBuilder;
 import org.cfg4j.source.files.FilesConfigurationSource;
@@ -19,6 +20,7 @@ public class ConfigReader {
 		ConfigurationProvider provider = new ConfigurationProviderBuilder().withConfigurationSource(source).build();
 
 		Config config = provider.bind(mountPoint, Config.class);
+		
 		return config;
 	}
 
@@ -31,7 +33,22 @@ public class ConfigReader {
 		ConfigurationProvider provider = new ConfigurationProviderBuilder().withConfigurationSource(source).build();
 
 		Config config = provider.bind("", Config.class);
+		
 		return config;
 	}
 
+	public static Config readConfigurationFromClassPath(String fileName){
+
+		ConfigFilesProvider configFilesProvider = () -> Arrays.asList(Paths.get(fileName));
+
+		// Use classpath as configuration store
+		ConfigurationSource source = new ClasspathConfigurationSource(configFilesProvider);
+
+		// Create provider
+		ConfigurationProvider provider = new ConfigurationProviderBuilder() .withConfigurationSource(source).build();
+		
+		Config config =  provider.bind("", Config.class);
+		
+		return config;
+	}
 }
